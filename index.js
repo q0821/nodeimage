8cced99d8e7585414e2453046a21b727c2218c27
Jackie Yeh <jackie@tellustek.com>
2017-07-20 00:58:26 +0800

    新增 room ，frame 參數
    1.調整router 套用不同room ， frame 來決定要合成哪個照片
    2.檔名使用timestamp+上傳上來的檔名再做MD5避免重複檔名
    
    todo
    1.product 跟 dev環境的參數檔
    2.移除上傳的原始照片

--- index.js
+++ index.js
@@ -1,45 +1,77 @@
-const fs = require('fs');
-const gm = require('gm').subClass({imageMagick: true});
-const path = require('path');
-const express = require('express');
-const fileUpload = require('express-fileupload');
-const app = express();
-// 使用 pug 當作template 
-app.set('view engine', 'pug');
-app.use(fileUpload());
+const gm = require('gm')
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+const morgan = require('morgan')
+const md5 = require('md5')
+const frameSetting = [
+  '235,140 1375, 885',
+  '115,150 1375, 885',
+  '1290,825 1375, 885',
+  '1330,125 1375, 885',
+  '1330,125 1375, 885'
+]
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-});
+// app.use(cors())
+app.use(function (req, res, next) {
+  // console.log('add header')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization')
+  // intercept OPTIONS method
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+})
+app.use(fileUpload())
+app.use(morgan('dev'))
+app.use('/file', express.static('./file'))
 
 // POST method route
-app.post('/send/', function (req, res) {
-  console.log(req.files);
-  if (!req.files)
-    return res.status(400).send('No files were uploaded.');
- 
+app.post('/send/:room/:frame', function (req, res) {
+  var room = req.params.room
+  var frame = req.params.frame
+  if (!req.files) {
+    return res.status(400).send('No files were uploaded.')
+  }
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file 
-  let sampleFile = req.files.sampleFile;
+  var sampleFile = req.files.slim_output_0
+  var filename = md5(new Date().getTime() + sampleFile.name)
   // Use the mv() method to place the file somewhere on your server 
-  console.log(sampleFile);
-  sampleFile.mv('./upload/filename.jpg', function(err) {
-    if (err){
-      return res.status(500).send(err);
+  console.log(sampleFile)
+  sampleFile.mv('./file/' + room + '/upload/' + filename + '.jpg', function (err) {
+    if (err) {
+      return res.status(500).send(err)
     }
-    gm('./resources/dev/images/help/b1.jpg')
-    .draw('image Over 233, 142 1374, 884 "./upload/filename.jpg"')
-    .write('./output/filename.jpg', function(err) {
-      if (!err) {
-          console.log('done');
+    gm('./file/' + room + '/resource/' + frame + '.jpg')
+      .draw('image Over ' + frameSetting[frame - 1] + ' "./file/' + room + '/upload/' + filename + '.jpg')
+      .write('./file/' + room + '/output/' + filename + '.jpg', function (err) {
+        if (!err) {
+          console.log('done')
           // res.sendFile('/output/filename.jpg');
-          res.sendFile(path.join(__dirname, './output', 'filename.jpg'));
-      } else { 
-        console.log(err.message || "出错了！"); 
-      }
-    });
-  });
-});
+          // res.sendFile(path.join(__dirname, './output', 'filename.jpg'));
+          // return res.send('oooo');
+          return res.json({
+            'status': 'success',
+            'name': filename + '.jpg',
+            'path': 'http://hd.tellustek.com:3000/file/' + room + '/output/' + filename + '.jpg'
+          })
+        } else {
+          console.log(err.message || '出错了！')
+          return res.json({
+            errcode: 1,
+            data: {
+              message: '出錯了！'
+            }
+          })
+        }
+      })
+  })
+})
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('Example app listening on port 3000!')
+})
